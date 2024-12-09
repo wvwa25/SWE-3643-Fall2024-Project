07a5aba101aeb848830cd55eadea9a4479a37f77
Vera Warren-Aliff <144066803+wvwa25@users.noreply.github.com>
2024-12-09 15:28:07 -0500

    Reconfiguration
    Reconfigured for Thymeleaf HTML rendering instead of JSTL/JSP

---
+++ src/main/java/main/controllers/MainController.java
@@ -0,0 +1,16 @@
+package main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String displayIndex(@RequestParam(name="name", required=false,
+			defaultValue="World") String name, Model model) {
+		return "index"; //
+	}
+}
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="html" %>
-
-<html>
-<head>
-<title> Howdy partner!</title>
-</head>
-<body>
-</body>
-</html>
